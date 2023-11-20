c4afe5fafed9a945ebd58c4dc5bd1bb5eb60d5ce
MinayeAliyeva <7s346pu@code.edu.az>
2023-11-21 00:08:59 +0400

    added changes

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,11 +1,13 @@
-import AboutTop from "./AboutTop/AboutTop"
+import AboutTop from "./AboutTop/AboutTop";
+import AboutPower from "./AboutPower/AboutPower";
 
 const About = () => {
   return (
     <>
-      <AboutTop/>
+      <AboutTop />
+      <AboutPower />
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
