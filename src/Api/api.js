088895a246ed8f363f645b3eb964b7df6e0e512a
TogrulEminov <togrul40@mail.ru>
2023-11-23 23:57:03 +0400

    fetch data from api in home page

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,20 +1,13 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://api.themoviedb.org/3';
-const TMDB_TOKEN =
-  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YzBmMTk4MDJkOTI4Y2FhNDk1MmFhNGFmNDU5YzJiYyIsInN1YiI6IjY1NWI0ZjBlN2YwNTQwMThkNDIwZWI2NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VCBNQylOrqRHikR8qYwjsGlxeYG8PleBKzMZHK6Tdoc';
+const BASE_URL = ' http://www.omdbapi.com/?i=tt3896198&apikey=84677991';
 
-const headers = {
-  Authorization: 'Bearer ' + TMDB_TOKEN,
-};
-
-export const fetchDataFromApi = async (url, params) => {
+export const fetchDataFromApi = async (url,params) => {
   try {
-    const { data } = await axios.get(BASE_URL + url, {
-      headers,
-      params,
+    const { data } = await axios.get(BASE_URL + url,{
+      params
     });
-   return data
+    return data;
   } catch (err) {
     console.log(err);
     return err;
--- src/Compnents/FilmCard/FilmCard.jsx
+++ src/Compnents/FilmCard/FilmCard.jsx
@@ -2,37 +2,21 @@ import React from 'react';
 import { Col } from 'react-bootstrap';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { Link } from 'react-router-dom';
-import "./FilmCard.scss"
-import useFetch from '../../hooks/useFetch';
-import { useSelector } from 'react-redux';
-const FilmCard = ({title,img}) => {
-  const { url } = useSelector((state) => state.api);
-  const { data, loading } = useFetch('/movie/popular');
-  console.log(data);
+import './FilmCard.scss';
+const FilmCard = ({ title, img }) => {
   return (
     <Col lg={3} xl={2} md={6} sm={6} xs={12}>
-      <Link to="" className="film_single_card">
+      <div to="" className="film_single_card">
         <figure className="film_single_image">
-          <LazyLoadImage
-            src="https://demo.gloriathemes.com/noxe/demo/wp-content/uploads/2020/02/title-poster-11-370x545.jpg"
-            alt="film_card"
-          />
+          <LazyLoadImage src={img} alt="film_card" />
         </figure>
-        <figcaption className="film_single_desc">
-          <Link className='film_single_title'>Darkness Lisa</Link>
-          <ul className='film_single_category'>
-            <li>
-              <Link>Adventure</Link>
-            </li>
-            <li>
-              <Link>Film-Noir</Link>
-            </li>
-            <li>
-              <Link>Thriller</Link>
-            </li>
-          </ul>
-        </figcaption>
-      </Link>
+        <div className="film_single_desc">
+          <Link to="" className="film_single_title">
+            {title}
+          </Link>
+          <ul className="film_single_category"></ul>
+        </div>
+      </div>
     </Col>
   );
 };
