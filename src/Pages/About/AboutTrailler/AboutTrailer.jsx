d3d234798ca2a9e9e3d4d6f22c1fac3b4b1acc98
TogrulEminov <togrul40@mail.ru>
2023-11-26 20:21:17 +0400

    add about trailler hover animation

--- src/Pages/About/AboutTrailler/AboutTrailer.jsx
+++ src/Pages/About/AboutTrailler/AboutTrailer.jsx
@@ -1,46 +1,62 @@
-import "./AboutTrailer.scss";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import trailler1 from "../../../Assets/Images/trailler1.webp";
-import trailler2 from "../../../Assets/Images/trailler2.webp";
-import trailler3 from "../../../Assets/Images/trailler3.webp";
+import './AboutTrailer.scss';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import trailler1 from '../../../Assets/Images/trailler1.webp';
+import trailler2 from '../../../Assets/Images/trailler2.webp';
+import trailler3 from '../../../Assets/Images/trailler3.webp';
+import { Link } from 'react-router-dom';
 const AboutTrailer = () => {
   return (
     <>
       <section id="about-trailler">
         <Row className="about-trailler">
           <Col className="col" xs={12} md={6}>
-            <a href="">
+            <div className="trailler_card">
               <img src={trailler1} alt="" />
               <div className="text">
-              <div className="content">
-              <h1>The Generations</h1>
-                <p>Action,Crime,Drama,Thriller</p>
+                <div className="content">
+                  <h1>The Generations</h1>
+                  <p>Action,Crime,Drama,Thriller</p>
+                </div>
               </div>
+              <div className="watch_trailler">
+                <Link to="" >
+                  Watch the Trailer
+                </Link>
               </div>
-            </a>
+            </div>
           </Col>
           <Col className="col" xs={12} md={6}>
-            <a href="">
+            <div className="trailler_card">
               <img src={trailler2} alt="" />
               <div className="text">
-              <div className="content">
-                <h1>Red Brothers</h1>
-                <p>Action,Family,Thriller</p>
+                <div className="content">
+                  <h1>Red Brothers</h1>
+                  <p>Action,Family,Thriller</p>
                 </div>
               </div>
-            </a>
+              <div className="watch_trailler">
+                <Link to="" >
+                  Watch the Trailer
+                </Link>
+              </div>
+            </div>
           </Col>
           <Col className="col" xs={12} md={6}>
-            <a href="">
+            <div className="trailler_card">
               <img src={trailler3} alt="" />
               <div className="text">
-              <div className="content">
-                <h1>The Gentlman</h1>
-                <p>Biography,Documentary,History</p>
+                <div className="content">
+                  <h1>The Gentlman</h1>
+                  <p>Biography,Documentary,History</p>
                 </div>
               </div>
-            </a>
+              <div className="watch_trailler">
+                <Link to="" >
+                  Watch the Trailer
+                </Link>
+              </div>
+            </div>
           </Col>
         </Row>
       </section>
