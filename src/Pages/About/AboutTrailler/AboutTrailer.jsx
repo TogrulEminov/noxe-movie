c91bd7551fce3ece65438de2f0ef8759add6908f
MinayeAliyeva <7s346pu@code.edu.az>
2023-11-26 18:04:03 +0400

    added styles

--- src/Pages/About/AboutTrailler/AboutTrailer.jsx
+++ src/Pages/About/AboutTrailler/AboutTrailer.jsx
@@ -9,13 +9,38 @@ const AboutTrailer = () => {
     <>
       <section id="about-trailler">
         <Row className="about-trailler">
-          <Col xs={12} md={6}>
-            <img src={trailler1} alt="" />
+          <Col className="col" xs={12} md={6}>
+            <a href="">
+              <img src={trailler1} alt="" />
+              <div className="text">
+              <div className="content">
+              <h1>The Generations</h1>
+                <p>Action,Crime,Drama,Thriller</p>
+              </div>
+              </div>
+            </a>
           </Col>
-          <Col xs={12} md={6}>
-          <img src={trailler2} alt="" /></Col>
-          <Col xs={12} md={6}>
-            <img src={trailler3} alt="" />
+          <Col className="col" xs={12} md={6}>
+            <a href="">
+              <img src={trailler2} alt="" />
+              <div className="text">
+              <div className="content">
+                <h1>Red Brothers</h1>
+                <p>Action,Family,Thriller</p>
+                </div>
+              </div>
+            </a>
+          </Col>
+          <Col className="col" xs={12} md={6}>
+            <a href="">
+              <img src={trailler3} alt="" />
+              <div className="text">
+              <div className="content">
+                <h1>The Gentlman</h1>
+                <p>Biography,Documentary,History</p>
+                </div>
+              </div>
+            </a>
           </Col>
         </Row>
       </section>
